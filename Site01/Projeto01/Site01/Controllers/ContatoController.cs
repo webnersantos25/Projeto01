f66359ac9d5d0f9cfa1081402663be40d2a40102
webnersantos25 <61808790+webnersantos25@users.noreply.github.com>
2021-10-06 18:46:10 -0300

    Add files via upload

---
+++ Site01/Projeto01/Site01/Controllers/ContatoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Site01.Biblioteca.Mail;
+using Site01.Models;
+namespace Site01.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Contato = new Contato();
+            return View();
+        } 
+
+        public IActionResult ReceberContato([FromForm] Contato contato)
+        {
+            if(ModelState.IsValid)
+            {
+                //string conteudo = string.Format("Nome: {0}, E-mail: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
+                // return new ContentResult() { Content = conteudo };
+                ViewBag.Contato = new Contato();
+                EnviarEmail.EnviarMensagemContato(contato);
+                ViewBag.Mensagem = "Mensagem enviada com sucesso";
+                return View("Index");
+                
+            }
+            else
+            {
+                ViewBag.Contato = contato;
+
+                return View("Index");
+            }
+            
+
+        }
+
+        /* Obter dados manualmente
+        public IActionResult Recebercontato()
+        {
+            Contato contato = new Contato();
+
+            contato.Nome = Request.Form["nome"];
+            contato.Email = Request.Form["email"];
+            contato.Assunto = Request.Form["assunto"];
+            contato.Mensagem = Request.Form["mensagem"];
+
+            string conteudo = string.Format("Nome: {0}, E-mail: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
+
+            return new ContentResult() { Content = conteudo };
+        }
+        */
+    }
+}
---
+++ Site01/Projeto01/Site01/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Site01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+
+        {
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login([FromForm]Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                if (usuario.Email == "teste.teste@gmail.com" && usuario.Senha == "5500")
+                {
+                    /*
+                       HttpContext.Session.SetString("Login", "true");
+                       HttpContext.Session.SetInt32("UserID", 32);
+
+                       string login = HttpContext.Session.GetString("Login");
+                       */
+                    HttpContext.Session.SetString("Login", "true");
+                    return RedirectToAction("index", "Palavra");
+                }
+                else
+                {
+                    ViewBag.Mensagem = "Os dados informados são inválidos!";
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+
+           
+        }
+        public ActionResult Logout()
+        {
+
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
