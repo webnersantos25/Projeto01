c7c931b1f31b48aeea1dcdf7bc5a5d86203507cf
fabriciopereira@bne-empregos.com.br <fabriciopereira@bne-empregos.com.br>
2021-10-13 21:47:40 -0300

    Corrigindo projetos Applicação

---
+++ Site01/Projeto01/Site01/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Site01.Database;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+namespace Site01
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<DatabaseContext>(options =>
+            {
+                //options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=site01;Integrated Security=True;");
+                options.UseSqlite("Data Source=Database\\site01.db");
+            });
+
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSession();
+
+            //código para acessar o arquivo de imagem através do wwwroot
+            app.UseStaticFiles();
+
+            //código para configurar o serviço de mvc
+            app.UseMvcWithDefaultRoute();
+
+            /*
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            }); */
+        }   
+    }
+}
